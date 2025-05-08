81e3fcb00b708bbb0be6f86d03172fd1b80bdb0d
erreyesjoel <erreyesjoel.gms@gmail.com>
2025-05-08 22:43:56 +0200

    curso de prueba en el seeder

--- src/database/seeders/CursosSeeder.php
+++ src/database/seeders/CursosSeeder.php
@@ -4,14 +4,22 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use App\Models\Curso;
 
 class CursosSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
-    public function run(): void
-    {
-        //
-    }
+  public function run(): void
+{
+    Curso::create([
+        'titulo' => 'curso 1',
+        'descripcion' => 'este es el curso 1',
+        'duracion' => '02:30:00',
+    ]);
+}
+
+
 }
