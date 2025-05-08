e485931d55665a3d307dbe1fb2bc231729d87923
Joel Erreyes <erreyesjoel.gms@gmail.com>
2025-05-08 22:25:04 +0200

    Primer commit del proyecto CursosOnline

---
+++ src/database/migrations/2025_05_08_195151_create_cursos_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('cursos', function (Blueprint $table) {
+            $table->id();
+            $table->string('titulo', 30);
+            $table->text('descripcion', 60);
+            $table->time('duracion');
+            $table->string('imagen_url', 255)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('cursos');
+    }
+};
