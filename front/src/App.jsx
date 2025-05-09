c9ac3f3d0d027771fe637275e5cbfe1e66b0e365
erreyesjoel <erreyesjoel.gms@gmail.com>
2025-05-10 00:40:38 +0200

    footer acabado

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,15 +1,20 @@
 import Header from './Header';
 import Cursos from './Cursos';
+import Footer from './Footer';
+import './index.css';
 
 const App = () => {
   return (
-    <>
+    <div className="layout">
       <Header />
-      <main className="contenido">
-        <Cursos />
-      </main>
-    </>
+      <div className="main-content">
+        <main className="contenido">
+          <Cursos />
+        </main>
+      </div>
+      <Footer />
+    </div>
   );
-}
+};
 
 export default App;
