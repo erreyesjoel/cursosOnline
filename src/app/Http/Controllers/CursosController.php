38ebf83dcac4f8a681064d133ba3a3e0ba8716c9
erreyesjoel <erreyesjoel.gms@gmail.com>
2025-05-09 12:00:25 +0200

    api get para coger todos los cursos

--- src/app/Http/Controllers/CursosController.php
+++ src/app/Http/Controllers/CursosController.php
@@ -2,9 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Curso;
 use Illuminate\Http\Request;
 
 class CursosController extends Controller
-{
-    //
+{   
+    // metodo para coger los cursos, y todas sus columnas
+    public function mostrarCursos()
+    {
+        return response()->json(Curso::all());
+    }
 }
