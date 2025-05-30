9696e621b319a84bb07458bc5683803d8b220075
erreyesjoel <erreyesjoel.gms@gmail.com>
2025-05-30 20:02:31 +0200

    fade out en notificaciones

--- front/src/Notificaciones.jsx
+++ front/src/Notificaciones.jsx
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './notificaciones.css';
 
 const Notificaciones = ({ notificacion }) => {
+  const [fade, setFade] = useState(false);
+
+  useEffect(() => {
+    if (notificacion) {
+      setFade(false);
+      const timer = setTimeout(() => setFade(true), 2000); // Empieza a desvanecer después de 2s
+      return () => clearTimeout(timer);
+    }
+  }, [notificacion]);
+
   if (!notificacion) return null;
   return (
-    <div className={`notification ${notificacion.tipo}`}>
+    <div className={`notification ${notificacion.tipo}${fade ? ' fade-out' : ''}`}>
       {notificacion.mensaje}
     </div>
   );
